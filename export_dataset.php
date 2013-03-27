b0982a5456f05aad8001f150767e3a06c750c232
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2013-03-27 17:06:58 -0500

    Add attempt at plain-text output
    Refs #50

---
+++ export_dataset.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Export a dataset as tab-delimited spreadsheet
+ *
+ * @package    local
+ * @subpackage dataseteditor
+ * @copyright  Daniel Seemuth
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/config.php');
+require_once(dirname(__FILE__) . '/../../config.php');
+require_once(dirname(__FILE__) . '/locallib.php');
+
+
+$categoryid = required_param('categoryid', PARAM_INT);
+
+$syscontext = context_system::instance();
+require_login($syscontext);
+
+require_capability('local/dataseteditor:view', $syscontext);
+
+$PAGE->set_url(PLUGINPREFIX.'/export_dataset.php', array('categoryid' => $categoryid));
+$PAGE->set_heading($SITE->fullname);
+$PAGE->set_title(
+    $SITE->fullname .
+    ': ' .
+    get_string('pluginname', 'local_dataseteditor') .
+    ': ' .
+    get_string('editdataset', 'local_dataseteditor')
+);
+$PAGE->set_pagelayout('incourse');
+
+$wildcards = get_wildcards($categoryid, 0); // Don't need any data values
+$items = get_dataset_items(array_keys($wildcards));
+
+header('Content-Type: text/plain');
+
+print_r($wildcards);
+print_r($items);
