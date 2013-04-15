7649374adc5dd16ec43857ded5a74ca58077d5fa
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2013-04-15 16:49:27 -0500

    Fix another arg order bug
    Refs #51

--- locallib.php
+++ locallib.php
@@ -344,8 +344,7 @@ function overwrite_wildcard_dataset(
      */
     $delete_ids = array();
     foreach ($cur_name2id as $name => $id) {
-        if (! array_key_exists($new_name2id, $name)) {
-            echo '<p>No exist ' . $name . ' (' . $id . ')</p>';
+        if (! array_key_exists($name, $new_name2id)) {
             $delete_ids[] = $id;
         }
     }
