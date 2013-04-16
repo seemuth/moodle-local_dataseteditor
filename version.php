234a3d621d01a850d0e57f40d38642babd67d690
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2013-04-16 10:35:59 -0500

    Remove old form class
    Refs #12

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2013041602;
+$plugin->version = 2013041603;
 $plugin->release = '0.1 (2013021901)';
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_ALPHA;
