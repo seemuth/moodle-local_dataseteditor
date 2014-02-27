ffcaa8e2566ef48a20dddc950e322cdf7e875f07
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2014-02-27 14:12:36 -0600

    Remove topcategory from navigation functions

--- lib.php
+++ lib.php
@@ -109,12 +109,6 @@ function local_dataseteditor_extends_navigation($nav) {
         $index_type = navigation_node::TYPE_CUSTOM;
     }
 
-    $topcategory = optional_param('topcategory', '', PARAM_SEQUENCE);
-
-    if ($topcategory) {
-        $urlparams['topcategory'] = $topcategory;
-    }
-
     $indexnode = $mainnode->add(
         get_string('pluginname', 'local_dataseteditor'),
         new moodle_url(
@@ -215,12 +209,6 @@ function local_dataseteditor_extends_settings_navigation($settings, $denode) {
         $index_type = navigation_node::TYPE_CONTAINER;
     } else {
         $index_type = navigation_node::TYPE_CUSTOM;
-    }
-
-    $topcategory = optional_param('topcategory', '', PARAM_SEQUENCE);
-
-    if ($topcategory) {
-        $urlparams['topcategory'] = $topcategory;
     }
 
     $indexnode = $questionbank->add(
