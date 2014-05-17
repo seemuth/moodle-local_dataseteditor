03922a497887506c8d912cdf4545c5a6530354f6
Daniel P. Seemuth <dps-github-moodle@seemuth.com>
2014-05-16 21:57:52 -0500

    Release v1.7.7: disallow duplicate wildcard names

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2014051600;
-$plugin->release = '1.7.6 (2014051600)';
+$plugin->version = 2014051601;
+$plugin->release = '1.7.7 (2014051601)';
 $plugin->requires = 2012120305;
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_dataseteditor';
